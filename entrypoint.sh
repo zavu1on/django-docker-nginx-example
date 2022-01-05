9a33e6fd9161c7d4b1fd780913d264b3259a1e08
Zavulon39 <64895285+Zavulon39@users.noreply.github.com>
2022-01-05 17:00:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-exec gunicorn DockerWeb.wsgi:application -b 0.0.0.0:8000
+exec gunicorn DockerWeb.wsgi:application -b 0.0.0.0:8000 -w 4
